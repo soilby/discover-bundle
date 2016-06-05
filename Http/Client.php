0e2ea09fe8b6606aa276f67ac67425a6f0a46ffd
George Novik <grgnvk@gmail.com>
2016-06-05 22:29:09 +0300

    Hardcode http auth params for all non production sites

--- Http/Client.php
+++ Http/Client.php
@@ -6,6 +6,17 @@ use EasyRdf\Http\Response;
 
 class Client extends EasyRdfHttpClient {
     public function request($method = null) {
+
+        $uri = $this->getUri();
+        if (strpos($uri, 'http://talaka.by') === false && strpos($uri, 'http://www.talaka.by') === false)   {
+
+            $username = "taluser";
+            $password = "Qe8hExas";
+            $basicAuthHash = base64_encode($username.":".$password);
+            
+            $this->setHeaders('Authorization', "Basic $basicAuthHash");
+        }
+
         $firstResponse = parent::request($method);
 
         $br = PHP_EOL;
